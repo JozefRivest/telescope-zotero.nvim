76b8d89b89110cecb0ce11a68377bdf767df1910
JozefRivest <jozef.rivest@umontreal.ca>
2025-03-02 12:16:49 -0500

    Corrections

--- lua/zotero/init.lua
+++ lua/zotero/init.lua
@@ -26,50 +26,23 @@ local default_opts = {
     },
     typst = {
       insert_key_formatter = function(citekey)
+        -- Prompt the user to choose between @citation and #cite(<citation>)
         local choices = {
           { label = '@citation', format = '@' .. citekey },
           { label = '#cite(<citation>)', format = '#cite(<' .. citekey .. '>)' },
         }
 
-        local popup = Popup {
-          enter = true,
-          focusable = true,
-          border = 'rounded',
-          position = '50%', -- Centers the window
-          size = {
-            width = 30,
-            height = 3,
-          },
-        }
-
-        -- Mount the popup
-        popup:mount()
-
-        -- Fill the popup with choices
-        vim.api.nvim_buf_set_lines(popup.bufnr, 0, -1, false, {
-          'Choose Typst citation format:',
-          '1. @citation',
-          '2. #cite(<citation>)',
-        })
-
-        -- Handle user input
-        vim.api.nvim_buf_set_keymap(popup.bufnr, 'n', '1', '', {
-          noremap = true,
-          silent = true,
-          callback = function()
-            popup:unmount()
-            vim.api.nvim_put({ choices[1].format }, '', false, true)
+        vim.ui.select(choices, {
+          prompt = 'Choose Typst citation format:',
+          format_item = function(choice)
+            return choice.label
           end,
-        })
-
-        vim.api.nvim_buf_set_keymap(popup.bufnr, 'n', '2', '', {
-          noremap = true,
-          silent = true,
-          callback = function()
-            popup:unmount()
-            vim.api.nvim_put({ choices[2].format }, '', false, true)
-          end,
-        })
+        }, function(selected)
+          if selected then
+            -- Insert the chosen citation format
+            vim.api.nvim_put({ selected.format }, '', false, true)
+          end
+        end)
       end,
       locate_bib = bib.locate_typst_bib,
     },
